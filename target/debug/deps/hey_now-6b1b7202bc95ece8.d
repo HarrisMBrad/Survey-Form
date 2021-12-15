d0c6ecde6ad8b2d9ef6f7946ab4d8ad790fb2900
HarrisMBrad <bmichaelh13@gmail.com>
2021-12-15 11:04:37 -0500

    rustic print
    added rust

---
+++ target/debug/deps/hey_now-6b1b7202bc95ece8.d
@@ -0,0 +1,5 @@
+C:\Users\brfor\hey-now\target\debug\deps\hey_now-6b1b7202bc95ece8.exe: src\main.rs
+
+C:\Users\brfor\hey-now\target\debug\deps\hey_now-6b1b7202bc95ece8.d: src\main.rs
+
+src\main.rs:
