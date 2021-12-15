e9855a07128adabf1563fb7accb58e2399361e93
HarrisMBrad <bmichaelh13@gmail.com>
2021-12-15 10:59:26 -0500

    added main macro match checking for Total on matches already made

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,28 @@ fn add(num_one: i32, num_two: i32) -> i32 {
 }
 
 fn main() {
-    let foo = add(10, 5);
+    let mut total = add(8, 20);
+    let mut free_shipping = false;
 
-    println!("{:?}", foo);
+    if total > 50 {
+        println!("You are qualified for free shipping!");
+        free_shipping = true;
+    } else if total > 20 {
+        println!(" If you add more items, you can qualify for free shipping!");
+    } else {
+        println!("No free shipping");
+    }
+
+    total = match free_shipping {
+        true => total + 0,
+        false => total + 5,
+    };
+
+    match total {
+        1 => println!("1"),
+        2 => println!("2"),
+        _ => println!("No Match Found: Time to Dev"),
+    }
+
+    println!("Total: {:?}", total);
 }
