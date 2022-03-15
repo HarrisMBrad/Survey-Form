91d4b602971cfabdae42a70cf5bd27c755283c9a
HarrisMBrad <bmichaelh13@gmail.com>
2022-03-15 13:15:43 -0400

    added files for contacts

---
+++ Contact.js
@@ -0,0 +1,47 @@
+const contactForm = document.getElementById("form");
+
+
+// person object these are key: 'value-pairs', 
+// thus they are called getters and setters in the dev community.
+// 
+const person = {
+  firstName: 'Tester',
+  lastName: 'Harris',
+  // calling this function inside the function means it's a method.
+  testName() {
+    return `${person.firstName} ${person.lastName}`;
+  }
+};
+
+console.log(person.testName());
+
+function contact() {
+
+  return ['', null];
+
+}
+contact([], console.log('null'));
+
+export default contactForm;
+
+// const fetchData = () => {
+//   fect('https://api.github.com').then(resp => {
+//     resp.json().then(data => {
+//       console.log(data);
+//     });
+//   });
+// };
+
+fectchData();
+
+async
+
+const fetchData = async () => {
+  const resp = await fetch('https://api.github.com');
+
+  const data = await resp.json();
+
+  console.log(data);
+};
+fecthData();
+
